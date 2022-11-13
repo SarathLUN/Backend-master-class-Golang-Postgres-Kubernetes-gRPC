98c6e8ae17e7e45136b9fa0517f49dfa47041506
SarathLUN <lun.sarath@gmail.com>
2022-11-13 11:22:25 +0700

    add database migration files

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,5 @@
+drop table if exists entries cascade;
+
+drop table if exists transfers cascade;
+
+drop table if exists accounts cascade;
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint,
+  "currency" varchar,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint not null ,
+  "amount" bigint,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint not null ,
+  "to_account_id" bigint not null ,
+  "amount" bigint,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be positive or negative';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
