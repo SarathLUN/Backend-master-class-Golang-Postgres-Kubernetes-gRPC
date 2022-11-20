8fb6a1c07b1a2b17c87e06520981e24ecebe4635
Sarath Lun <lun.sarath@ezecomcorp.com>
2022-11-20 10:17:45 +0700

    add store file

---
+++ db/sqlc/store.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store will provide all functions to execute db query and transaction.
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %v, rollback error: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
